8b73d7e62cfc5284eb53732b42098da15942c2be
Edgar <epgarzonr@uce.edu.ec>
2025-01-28 20:41:24 -0500

    feat(service): Implement EmailService to send survey emails

---
+++ internal/service/email_service.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+    "email-service/internal/models"
+)
+
+type EmailService struct {
+    mailer *mail.Mailer
+}
+
+func NewEmailService(mailer *mail.Mailer) *EmailService {
+    return &EmailService{mailer: mailer}
+}
+
+func (s *EmailService) SendSurveyEmail(req models.EmailRequest) error {
+    survey := models.SurveyEmail{
+        To:         req.Email,
+        Name:       req.Name,
+        Restaurant: req.Restaurant,
+    }
+    
+    return s.mailer.SendSurvey(survey)
+}
